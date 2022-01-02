eac98fe3ae18b1f2b164207fbbe73a1a700b3a12
Edric Chan <edric.chan.1997@gmail.com>
2022-01-02 18:33:35 +0800

    backend: Move Prisma NestJS integration to dedicated subfolder

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { PrismaService } from './services/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
-    );
+  );
+
+  // See https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
   await app.listen(3001);
 }
 bootstrap();
